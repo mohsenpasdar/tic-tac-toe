277b1e9781bc122091794c2353da7e37eea83351
Mohsen Pasdar <mohsenpasdar@gmail.com>
2022-05-22 15:34:41 +0430

    working ..., reset button is not working yet.

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -8,7 +8,11 @@ const initial_moves = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 const TicTacToe = () => {
 
-    const [players, dispatch] = useReducer(reducer, { player1: initial_moves, player2: initial_moves, disabled: false});
+    const [players, dispatch] = useReducer(reducer, { player1: initial_moves, player2: initial_moves, resetMoves: false, resetGame: false});
+
+    const resetGame = () => {
+      players.resetGame = false
+    }
 
     const checkWinner = (array) => {
       if ((array[0] == 1 && array[1] == 1 && array[2] == 1) ||
@@ -19,6 +23,10 @@ const TicTacToe = () => {
           (array[2] == 1 && array[5] == 1 && array[8] == 1) ||
           (array[0] == 1 && array[4] == 1 && array[8] == 1) ||
           (array[2] == 1 && array[4] == 1 && array[6] == 1)) {
+            players.resetMoves = true
+            players.resetGame = true
+            players.player1 = initial_moves
+            players.player2 = initial_moves
             return true
           }
     }
@@ -29,6 +37,12 @@ const TicTacToe = () => {
             <Board />
             <h1>{checkWinner(players.player1) ? 'player1 won!' : ''}</h1>
             <h1>{checkWinner(players.player2) ? 'player2 won!' : ''}</h1>
+            <p>{players.player1}</p>
+            <p>{players.player2}</p>
+            <p>{players.resetMoves ? 'resetmoves is true' : 'resetmoves is false'}</p>
+            <p>{players.resetGame ? 'resetGame is true' : 'resetGame is false'}</p>
+            {players.resetGame ? <button onClick={resetGame}>reset game</button> : 'resetgame offff'}
+
         </TicTacToeContext.Provider>
     )
 }
